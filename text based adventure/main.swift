229c9b3509d5a82a5fe2768c19b7d45727604164
Robert Calungsod <tribucalungsod@Roberts-iMac.local>
2017-08-17 12:30:46 -0400

    Initial Commit

---
+++ text based adventure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  text based adventure
+//
+//  Created by Robert Calungsod on 8/17/17.
+//  Copyright © 2017 Erikson Calungsod. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
